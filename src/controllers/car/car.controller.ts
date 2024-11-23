95a78fb0b6d2cda63642db9a5eb6a6fb0b51540c
Nahuel Monje <102997844+monjenahuel@users.noreply.github.com>
2024-11-23 19:09:37 -0300

    Nuevo endpoint para verificar disponibilidad de un particular auto entre 2 fechas

--- src/controllers/car/car.controller.ts
+++ src/controllers/car/car.controller.ts
@@ -5,6 +5,7 @@ import { FiltrosDTO } from 'src/models/DTO/FiltrosDTO';
 import { combustibleEnum } from 'src/models/enums/CombustibleEnum';
 import { TransmisionEnum } from 'src/models/enums/TransmisionEnum';
 import { CarDTO } from 'src/models/DTO/CarDTO';
+import {AvailabilityDTO} from "../../models/DTO/AvailabilityDTO";
 
 
 @Controller()
@@ -55,6 +56,12 @@ export class CarController {
   @Get("withdeleted/car/:id")
   getCarByIdWithDeleted(@Param('id') id: number): Promise<Car> {
     return this.carService.getCarByIdWithDeleted(id);
+  }
+
+  @Post('/car/:id/available')
+  getCarAvailabilityById(@Param('id') id: number, @Body() filtros:FiltrosDTO): Promise<AvailabilityDTO> {
+    console.log("--- car.controller : Verificando disponibilidad del auto ID: ", id, "filtros: ", filtros);
+    return this.carService.getCarAvailabilityById(id,filtros);
   }
 
 }
